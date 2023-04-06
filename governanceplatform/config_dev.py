a14d77e4b652271be89de602a5d91a04254fb5a2
jerolomb <jerome.lombardi@cases.lu>
2023-04-06 13:19:05 +0200

    first draft for poetry conf file, adding file for configuration

---
+++ governanceplatform/config_dev.py
@@ -0,0 +1,74 @@
+import os
+
+PUBLIC_URL = ""
+ALLOWED_HOSTS = ["127.0.0.1", locals().get("PUBLIC_URL", "")]
+OPERATOR_CONTACT = {
+    "name": "Organization Name",
+    "street": "Organization Street",
+    "zip_code": "Organization Zip Code",
+    "country": "Organization Country",
+    "phone": "Organization Phone Number",
+    "website": "https://www.example.org",
+    "contact_email": "contact@example.org",
+    "privacy_email": "privacy@exemple.org",
+}
+
+# The generic site/tool name. Used to load specific config, templates, styles, logo.
+SITE_NAME = "governanceplatform"
+
+SECRET_KEY = "django-insecure-1*nt5exfgm+po13ngrz7fm5sitfvi24f!13t=z*l5zz*w9zic$"
+
+HASH_KEY = b"#StandWithtUkraineHP-TmGv-4z7h-1xaQp0RYuY20="
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGIN_REGEXES = []
+CORS_ALLOW_METHODS = [
+    "GET",
+    "OPTIONS",
+]
+
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 25
+
+# Logging mechanism
+LOG_DIRECTORY = "./logs"
+LOG_FILE = "django.log"
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(LOG_DIRECTORY, LOG_FILE),
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["file"], "level": "INFO", "propagate": True},
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                "%(asctime)s [%(levelname)-8s] (%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
+
+
--- governanceplatform/settings.py
+++ governanceplatform/settings.py
@@ -11,21 +11,58 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import importlib
+import os
+import sys
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+try:
+    from governanceplatform import config  # type: ignore
+except ImportError:  # pragma: no cover
+    from governanceplatform import config_dev as config
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = config.SECRET_KEY
+    HASH_KEY = config.HASH_KEY
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = config.DEBUG
+    LOGGING = config.LOGGING
+    LOG_DIRECTORY = config.LOG_DIRECTORY
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+    ALLOWED_HOSTS = config.ALLOWED_HOSTS
+    PUBLIC_URL = config.PUBLIC_URL
+    OPERATOR_CONTACT = config.OPERATOR_CONTACT
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1*nt5exfgm+po13ngrz7fm5sitfvi24f!13t=z*l5zz*w9zic$'
+    EMAIL_HOST = config.EMAIL_HOST
+    EMAIL_PORT = config.EMAIL_PORT
+except AttributeError as e:
+    print("Please check you configuration file for the missing configuration variable:")
+    print(f"  {e}")
+    exit(1)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+try:
+    CORS_ALLOWED_ORIGINS = config.CORS_ALLOWED_ORIGINS
+    CORS_ALLOWED_ORIGIN_REGEXES = config.CORS_ALLOWED_ORIGIN_REGEXES
+    CORS_ALLOW_METHODS = config.CORS_ALLOW_METHODS
+except AttributeError:
+    CORS_ALLOWED_ORIGINS = []
+    CORS_ALLOWED_ORIGIN_REGEXES = []
+    CORS_ALLOW_METHODS = []
 
-ALLOWED_HOSTS = []
+try:
+    if LOG_DIRECTORY:
+        # if not logging in stdout
+        os.makedirs(LOG_DIRECTORY, exist_ok=True)
+except Exception as e:
+    print("Impossible to create the log directory:")
+    print(f"  {e}")
+    exit(1)
 
 
 # Application definition
@@ -37,11 +74,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.sites",
     'regulator',
     'operateur',
+    "drf_spectacular",
+    "drf_spectacular_sidecar",  # required for Django collectstatic discovery
+    "corsheaders",
+]
+
+context_processors = [
+    "django.template.context_processors.request",
+    "django.contrib.auth.context_processors.auth",
+    "django.contrib.messages.context_processors.messages",
+    "survey.context_processors.get_version",
+    "survey.context_processors.instance_configurations",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,22 +99,35 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
-ROOT_URLCONF = 'governanceplatform.urls'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+if DEBUG:
+    INSTALLED_APPS.append("debug_toolbar")
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+    context_processors.append("django.template.context_processors.debug")
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+
+
+ROOT_URLCONF = "governanceplatform.urls"
+
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            'templates'
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": context_processors,
         },
     },
 ]
@@ -72,16 +135,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'governanceplatform.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -107,19 +161,27 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Paris'
-
+TIME_ZONE = "UTC"
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+SITE_ID = 1
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 
-STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Used to get an access to the header on JS side.
+CORS_EXPOSE_HEADERS = [
+    "content-disposition",
+]
+
+
