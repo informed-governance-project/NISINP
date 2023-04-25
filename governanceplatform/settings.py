2017b6291d0c0e63ee2fdc7212880ea87a099a49
Juan Rocha <juan.rocha@nc3.lu>
2023-04-25 08:49:15 +0200

    [translate]Added first draft language selector

--- governanceplatform/settings.py
+++ governanceplatform/settings.py
@@ -176,6 +176,12 @@ USE_L10N = True
 USE_TZ = True
 SITE_ID = 1
 
+LANGUAGES = [
+    ('en', 'English'),
+    ('fr', 'Français'),
+    ('nl', 'Dutch'),
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
