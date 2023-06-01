ec1475098708ea4c98d9ae056c9512fa96c43571
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-06-01 15:01:45 +0200

    chg: [proxy] set default port fo / to 5001

--- proxy/urls.py
+++ proxy/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from revproxy.views import ProxyView
 
 urlpatterns = [
-    re_path(r"(?P<path>.*)", ProxyView.as_view(upstream="http://example.com/")),
+    re_path(r"(?P<path>.*)", ProxyView.as_view(upstream="http://127.0.0.1:5001/")),
     re_path(
         r"^notifications/(?P<path>.*)$",
         ProxyView.as_view(upstream="http://127.0.0.1:5002/"),
