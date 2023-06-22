3795f26b14201e40722f045a4da7578f76e9e6f2
Juan Rocha <juan.rocha@nc3.lu>
2023-06-22 10:30:33 +0200

    [cleanup]Removed proxy useless code

--- proxy/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from governanceplatform.decorators import operateur_required
-
-
-@operateur_required
-def index(request):
-    return render(request, "operateur/index.html")
