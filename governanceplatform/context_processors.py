ff66adbabbe88aa4d9a43f638bf16636f0ddbecc
Juan Rocha <juan.rocha@nc3.lu>
2023-06-28 08:48:09 +0200

    [admin]Added missing context processors

--- governanceplatform/context_processors.py
+++ governanceplatform/context_processors.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from .models import Company
 from .settings import SITE_NAME
 
@@ -7,6 +9,8 @@ def extra_content_for_all_templates(request):
 
     if request.user.is_superuser:
         extra_data["template_header"] = "admin/base_site.html"
+        extra_data["site_header"] = SITE_NAME + " " + _("Administration")
+        extra_data["site_title"] = SITE_NAME
 
     if request.user.is_authenticated and request.user.companies.exists():
         extra_data["is_regulator"] = request.user.companies.first().is_regulator
