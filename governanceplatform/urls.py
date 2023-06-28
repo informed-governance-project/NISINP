ee49a5fc99d0e0fc4f8debd97fe98d348079e896
Juan Rocha <juan.rocha@nc3.lu>
2023-06-28 08:50:19 +0200

    [modules]Removed proxy stuff

--- governanceplatform/urls.py
+++ governanceplatform/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.views.i18n import set_language
-from revproxy.views import ProxyView
 from two_factor.urls import urlpatterns as tf_urls
 from two_factor.views import LoginView
 
@@ -52,17 +51,6 @@ urlpatterns = [
     path("privacy/", views.privacy, name="privacy"),
     # Language Selector
     path("set-language/", set_language, name="set_language"),
-    # Proxy views
-    # re_path(r'casesmodels', ProxyView.as_view(upstream='http://127.0.0.1:5001/')),
-    # re_path(r"^(?P<path>.*)$", ProxyView.as_view(upstream="http://127.0.0.1:5001/")),
-    re_path(
-        r"^monarc/(?P<path>.*)$", ProxyView.as_view(upstream="http://127.0.0.1:5001/")
-    ),
-    re_path(r"^bo/(?P<path>.*)$", ProxyView.as_view(upstream="http://127.0.0.1:5000/")),
-    re_path(
-        r"^notifications/(?P<path>.*)$",
-        ProxyView.as_view(upstream="http://127.0.0.1:5002/"),
-    ),
 ]
 
 if DEBUG:
