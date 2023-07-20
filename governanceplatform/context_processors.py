b85c163b38b94b2f495e4228f1dab5f952edb113
Juan Rocha <juan.rocha@nc3.lu>
2023-07-20 13:43:10 +0200

    [admin]Adjust context with permission groups

--- governanceplatform/context_processors.py
+++ governanceplatform/context_processors.py
@@ -1,21 +1,22 @@
 from django.utils.translation import gettext_lazy as _
 
+from .helpers import user_in_group
 from .models import Company
 from .settings import SITE_NAME
 
 
 def extra_content_for_all_templates(request):
+    user = request.user
     extra_data = {"site_name": SITE_NAME}
-    extra_data["is_staff"] = request.user.is_staff
-    extra_data["is_superuser"] = request.user.is_superuser
 
-    if request.user.is_superuser:
+    if user_in_group(user, "PlatformAdmin") or user_in_group(user, "RegulatorAdmin"):
+        extra_data["only_admin"] = True
         extra_data["template_header"] = "admin/base_site.html"
         extra_data["site_header"] = SITE_NAME + " " + _("Administration")
         extra_data["site_title"] = SITE_NAME
 
-    if request.user.is_authenticated and request.user.companies.exists():
-        extra_data["is_regulator"] = request.user.companies.first().is_regulator
+    if user.is_authenticated and user.companies.exists():
+        extra_data["is_regulator"] = user.companies.first().is_regulator
 
     company_in_use = request.session.get("company_in_use")
     if company_in_use:
