27575c7ad8738d0d1782dd900da0c4bce097630f
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-09-13 21:54:43 +0200

    updated admin.py

--- governanceplatform/admin.py
+++ governanceplatform/admin.py
@@ -12,6 +12,8 @@ from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 from parler.admin import TranslatableAdmin
 from parler.models import TranslationDoesNotExist
 
+from incidents.models import Impact
+
 from .helpers import user_in_group
 from .models import (
     Company,
@@ -637,3 +639,21 @@ class OperatorTypeAdmin(ImportExportModelAdmin, TranslatableAdmin):
     list_display = ["type"]
     search_fields = ["type"]
     resource_class = OperatorTypeResource
+
+
+class ImpactResource(resources.ModelResource):
+    id = fields.Field(
+        column_name="id",
+        attribute="id",
+    )
+
+    class Meta:
+        model = Impact
+
+
+@admin.register(Impact, site=admin_site)
+class ImpactAdmin(admin.ModelAdmin):
+    list_display = [
+        "is_generic_impact",
+    ]
+    resource_class = ImpactResource
