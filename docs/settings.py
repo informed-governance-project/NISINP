32db01dd5ae59cabb061b3ba51be107951f50e4c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-09-17 22:58:34 +0200

    chg: [documentation] test autodoc type hints !minor

--- docs/settings.py
+++ docs/settings.py
@@ -15,6 +15,40 @@ DATABASES = {
     }
 }
 
+try:
+    from governanceplatform import config  # type: ignore
+except ImportError:  # pragma: no cover
+    from governanceplatform import config_dev as config
+
+
+try:
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = config.SECRET_KEY
+    HASH_KEY = config.HASH_KEY
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = config.DEBUG
+    LOGGING = config.LOGGING
+    LOG_DIRECTORY = config.LOG_DIRECTORY
+
+    ALLOWED_HOSTS = config.ALLOWED_HOSTS
+    PUBLIC_URL = config.PUBLIC_URL
+    REGULATOR_CONTACT = config.REGULATOR_CONTACT
+    SITE_NAME = config.SITE_NAME
+
+    EMAIL_HOST = config.EMAIL_HOST
+    EMAIL_PORT = config.EMAIL_PORT
+    EMAIL_SENDER = config.EMAIL_SENDER
+    DEFAULT_FROM_EMAIL = config.EMAIL_SENDER
+
+    MAX_PRELIMINARY_NOTIFICATION_PER_DAY_PER_USER = (
+        config.MAX_PRELIMINARY_NOTIFICATION_PER_DAY_PER_USER
+    )
+except AttributeError as e:
+    print("Please check you configuration file for the missing configuration variable:")
+    print(f"  {e}")
+    exit(1)
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
