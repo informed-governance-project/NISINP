168641887bfbaedefbf5935b25d08211ea03e2ee
Ruslan Baidan <ruslan.baidan@nc3.lu>
2023-09-22 09:47:01 +0200

    Removed the unused import.

--- incidents/views.py
+++ incidents/views.py
@@ -5,7 +5,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
-from django.forms import formset_factory
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils.translation import gettext as _
