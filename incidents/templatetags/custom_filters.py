4785e3a1e2d86f7498eb1f9902f83bbacde67351
Juan Rocha <juan.rocha@nc3.lu>
2023-12-04 16:25:40 +0100

    [incident]Improved incident list on operator and regulator

--- incidents/templatetags/custom_filters.py
+++ incidents/templatetags/custom_filters.py
@@ -22,3 +22,26 @@ def index(indexable, i):
 @register.filter()
 def translate(text):
     return _(text)
+
+
+@register.simple_tag
+def status_class(value):
+    if value == "PASS":
+        return "table-success"
+    elif value == "FAIL":
+        return "table-danger"
+    elif value == "DELIV":
+        return "table-info"
+    elif value == "OUT":
+        return "table-secondary"
+    else:
+        return ""
+
+
+@register.filter
+def filter_workflows(incidentWorkflows, report_id):
+    return [
+        incidentworkflow
+        for incidentworkflow in incidentWorkflows
+        if incidentworkflow.workflow.pk == report_id
+    ]
