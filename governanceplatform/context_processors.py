b9a5f1562c43c924efdc3c771fe8c28a87376cc9
Juan Rocha <juan.rocha@nc3.lu>
2023-12-06 14:27:51 +0100

    [context]Fixed processors

--- governanceplatform/context_processors.py
+++ governanceplatform/context_processors.py
@@ -1,19 +1,22 @@
 from django.utils.translation import gettext_lazy as _
 
-from .helpers import user_in_group, is_user_regulator
+from .helpers import is_user_regulator, user_in_group
 from .settings import SITE_NAME
 
 
 def extra_content_for_all_templates(request):
     user = request.user
-    extra_data = {"site_name": SITE_NAME, "only_admin": False}
+    extra_data = {
+        "site_name": SITE_NAME,
+        "is_staff": user.is_staff,
+        "is_only_admin": False,
+    }
+    extra_data["is_regulator"] = is_user_regulator(user)
 
-    if user_in_group(user, "PlatformAdmin") or user_in_group(user, "RegulatorAdmin"):
-        extra_data["only_admin"] = True
+    if user_in_group(user, "PlatformAdmin"):
+        extra_data["is_only_admin"] = True
         extra_data["template_header"] = "admin/base_site.html"
         extra_data["site_header"] = SITE_NAME + " " + _("Administration")
         extra_data["site_title"] = SITE_NAME
-
-    extra_data["is_regulator"] = is_user_regulator(user)
 
     return extra_data
