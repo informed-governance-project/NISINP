17b954c9e6301d59a7ee7711fa69cd20584901e5
Juan Rocha <juan.rocha@nc3.lu>
2023-12-06 14:34:58 +0100

    [filters]Added new filter and tags for incident view

--- incidents/templatetags/custom_filters.py
+++ incidents/templatetags/custom_filters.py
@@ -45,3 +45,27 @@ def filter_workflows(incidentWorkflows, report_id):
         for incidentworkflow in incidentWorkflows
         if incidentworkflow.workflow.pk == report_id
     ]
+
+
+@register.simple_tag
+def is_workflow_disabled(allWorkflows, incidentWorkflows, report):
+    current_index = allWorkflows.index(report)
+
+    if not incidentWorkflows and not current_index == 0:
+        return True
+
+    workflow_list = [workflow.workflow for workflow in incidentWorkflows]
+
+    if (
+        current_index < len(allWorkflows) - 1
+        and allWorkflows[current_index + 1] in workflow_list
+    ):
+        return True
+
+    if (
+        current_index == len(allWorkflows) - 1
+        and allWorkflows[current_index - 1] not in workflow_list
+    ):
+        return True
+
+    return False
