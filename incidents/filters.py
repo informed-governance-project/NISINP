905f9f2500fa26cff4b88c73ee897e99cc83401e
jerolomb <jerome.lombardi@cases.lu>
2024-01-23 09:28:20 +0100

    add missing file comment #88

---
+++ incidents/filters.py
@@ -0,0 +1,27 @@
+import django_filters
+from .models import Incident
+from .forms import IncidentWorkflowForm, IncidentStatusForm
+
+
+class IncidentFilter(django_filters.FilterSet):
+    incident_id = django_filters.CharFilter(lookup_expr='iexact')
+
+    class Meta:
+        model = Incident
+        fields = ['incident_id', 'incident_status']
+
+    @property
+    def qs(self):
+        parent = super().qs
+
+        for incident in parent:
+            incident.formsWorkflow = []
+            for workflow_completed in incident.get_workflows_completed():
+                incident.formsWorkflow.append(
+                    IncidentWorkflowForm(instance=workflow_completed)
+                )
+            incident.formsStatus = IncidentStatusForm(
+                instance=incident,
+            )
+
+        return parent
