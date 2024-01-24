f3b50596439ff2e9fe3aa297058d9a4e77f177b4
jerolomb <jerome.lombardi@cases.lu>
2024-01-24 13:56:04 +0100

    add filter for workflow comment #88

--- incidents/filters.py
+++ incidents/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import Incident, SectorRegulation
 from governanceplatform.models import Sector
-from .forms import IncidentWorkflowForm, IncidentStatusForm
+from .forms import IncidentWorkflowForm, IncidentStatusForm, DropdownCheckboxSelectMultiple
 from django.db.models.functions import Concat
 from django.db.models import Case, Value, When
 from django.utils.translation import get_language
@@ -10,24 +10,36 @@ from django.utils.translation import get_language
 # define a tree view for the sectors (only work with 2 levels)
 # Only fetch on the current language
 def affected_sectors(request):
-
-    return Sector.objects.translated(get_language()).annotate(
-        full_name=Case(
-            When(parent__isnull=False, then=Concat('parent__translations__name', Value(' --> '), 'translations__name')),
-            default='translations__name',
+    return (
+        Sector.objects.translated(get_language())
+        .annotate(
+            full_name=Case(
+                When(
+                    parent__isnull=False,
+                    then=Concat(
+                        "parent__translations__name",
+                        Value(" --> "),
+                        "translations__name",
+                    ),
+                ),
+                default="translations__name",
+            )
         )
-    ).order_by('full_name')
+        .order_by("full_name")
+    )
 
 
 # define specific query to get the regulation
-# TO DO : the returned value is not correct
 def sector_regulation(request):
-    return SectorRegulation.objects.all().values_list('regulation__translations__label', flat=True).distinct()
+    return SectorRegulation.objects.translated(get_language()).distinct()
 
 
 class IncidentFilter(django_filters.FilterSet):
-    incident_id = django_filters.CharFilter(lookup_expr='icontains')
-    affected_sectors = django_filters.ModelChoiceFilter(queryset=affected_sectors)
+    incident_id = django_filters.CharFilter(lookup_expr="icontains")
+    affected_sectors = django_filters.ModelMultipleChoiceFilter(
+        queryset=affected_sectors,
+        widget=DropdownCheckboxSelectMultiple()
+    )
     sector_regulation = django_filters.ModelChoiceFilter(queryset=sector_regulation)
 
     class Meta:
@@ -37,7 +49,7 @@ class IncidentFilter(django_filters.FilterSet):
             "incident_status",
             "is_significative_impact",
             "affected_sectors",
-            "sector_regulation"
+            "sector_regulation",
         ]
 
     # Needed to add the form for modification of
