c5fee3f7a59a201ec4cbdb195f99db54e509d4d5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2024-01-29 14:12:39 +0100

    chg: [JavaScript] Removed use deprectaed JS features.

--- static/js/language-selector.js
+++ static/js/language-selector.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $('#language_selector').change(function () {
-        $(this).closest('form').submit();
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("language_selector").onchange(function () {
+        document.getElementById("language_selector").parent().submit();
+    })
 });
