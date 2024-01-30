74d7f3808e7d2881256aa18b9508e30a5621f7e9
Cédric Bonhomme <cedric@cedricbonhomme.org>
2024-01-30 09:47:23 +0100

    fix: [templates] Minor fix related to the DataTable.

--- static/js/incidents_regulator.js
+++ static/js/incidents_regulator.js
@@ -83,51 +83,38 @@ function getReviewStatusClass(reviewStatus) {
 }
 
 
-// $(document).ready(function () {
-//     new DataTable('#incidents-table', {
-//         columnDefs: [{
-//             targets: 0,
-//             type: 'mydate'
-//         },
-//         {
-//             targets: 2,
-//             type: 'mydate'
-//         }
-//     ]
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    Object.assign($.fn.DataTable.ext.oSort, {
+        'mydate-asc': (a,b) => new Date(a) - new Date(b),
+        'mydate-desc': (a,b) => new Date(b) - new Date(a)
+    });
 
-
-  $('#incidents-table').DataTable( {
-    paging: false,
-    searching: false,
-    order: [[0, 'mydate-desc']],
-    columnDefs: [
-        {
-            targets: 0,
-            type:'my-date'
-        },
-        {
-            targets: 2,
-            orderable: true,
-            type:'string'
-        },
-        {
-            targets: 7,
-            orderable: false,
-        },
-        {
-            targets: 8,
-            orderable: false,
-        },
-        {
-            targets: 9,
-            orderable: false,
-        },
-    ]
-} );
-
-Object.assign($.fn.DataTable.ext.oSort, {
-    'mydate-asc': (a,b) => new Date(a) - new Date(b),
-    'mydate-desc': (a,b) => new Date(b) - new Date(a)
-  });
+    let table = new DataTable('#incidents-table', {
+        paging: false,
+        searching: false,
+        // order: [[0, 'mydate-desc']],
+        columnDefs: [
+            {
+                targets: 0,
+                type:'my-date'
+            },
+            {
+                targets: 2,
+                orderable: true,
+                type:'string'
+            },
+            {
+                targets: 7,
+                orderable: false,
+            },
+            {
+                targets: 8,
+                orderable: false,
+            },
+            {
+                targets: 9,
+                orderable: false,
+            },
+        ]
+    });
+});
