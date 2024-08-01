77cf46bffe7e786a1bbfb3b9c1d2f6c406bb980f
Juan Rocha <juan.rocha@nc3.lu>
2024-08-01 15:24:13 +0200

    [static]Moved JS to theme

--- static/js/incidents_operator.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.workflow_comment').on( "click", function() {
-        let $this = $(this);
-        let workflowComment = $this.data('workflow-comment');
-        let $modalWorkflowComment = $('#modal-workflow-comment');
-        $modalWorkflowComment.text(workflowComment);
-    });
-});
