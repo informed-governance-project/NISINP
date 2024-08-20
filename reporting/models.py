18413380dc01bc56e5140e9becb860bef00509be
jerolomb <jerome.lombardi@cases.lu>
2024-08-20 11:48:36 +0200

    modification of model

--- reporting/models.py
+++ reporting/models.py
@@ -19,26 +19,6 @@ class RiskAnalysisJson(models.Model):
     timestamp = models.DateTimeField(verbose_name=_("Timestamp"), default=timezone.now)
 
 
-# scales information
-class ScaleData(TranslatableModel):
-    translations = TranslatedFields(
-        name=models.CharField(
-            verbose_name=_("Name"),
-            max_length=255,
-            blank=True,
-            default=None,
-            null=True,
-        ),
-    )
-    minimum = models.IntegerField(verbose_name=_("Minimum"))
-    maximum = models.IntegerField(verbose_name=_("Maximum"))
-    risk_analysis = models.ForeignKey(
-        RiskAnalysisJson,
-        on_delete=models.CASCADE,
-        verbose_name=_("Risk Analysis"),
-    )
-
-
 # Store the statistic by sector, should be updated each time a company from the sector changed
 class SectorStatistic(models.Model):
     sector = models.ForeignKey(
@@ -55,23 +35,28 @@ class SectorStatistic(models.Model):
     )
 
     class Meta:
-        verbose_name_plural = _("Secrtor stats")
+        verbose_name_plural = _("Sector stats")
         verbose_name = _("Sector stat")
 
 
 # Store the stat for each service in a risk analysis
-class ServiceStat(models.Model):
-    service_name = models.CharField(
-        verbose_name=_("Service name"),
-        max_length=255,
+class ServiceStat(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(
+            verbose_name=_("Name"),
+            max_length=255,
+            blank=True,
+            default=None,
+            null=True,
+        ),
     )
     risk_analysis = models.ForeignKey(
         RiskAnalysisJson,
         on_delete=models.CASCADE,
         verbose_name=_("Risk Analysis"),
     )
-    treshold_for_high_risk = models.FloatField(
-        verbose_name=_("High risk rate treshold"),
+    threshold_for_high_risk = models.FloatField(
+        verbose_name=_("High risk rate threshold"),
     )
     high_risk_rate = models.FloatField(
         verbose_name=_("High risk rate"),
@@ -86,39 +71,37 @@ class ServiceStat(models.Model):
 
 
 # store asset
-class AssetData(models.Model):
-    name = models.CharField(
-        max_length=255,
-        verbose_name=_("Asset name"),
+class AssetData(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(
+            verbose_name=_("Name"),
+            max_length=255,
+            blank=True,
+            default=None,
+            null=True,
+        ),
     )
-    asset_type_uuid = models.CharField(
-        max_length=32,
-        verbose_name=_("Asset Type uuid"),
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        verbose_name=_("uuid"),
     )
-    impacts = models.ManyToManyField(
-        ScaleData, through="AssetScale", verbose_name=_("Scales value")
+    impact_c = models.FloatField(
+        verbose_name=_("Confidentility impact"),
+        default=-1,
+    )
+    impact_i = models.FloatField(
+        verbose_name=_("Integrity impact"),
+        default=-1,
+    )
+    impact_a = models.FloatField(
+        verbose_name=_("Availability impact"),
+        default=-1,
     )
 
     class Meta:
         verbose_name_plural = _("Asset")
         verbose_name = _("Assets")
-
-
-# impact for assets
-class AssetScale(models.Model):
-    asset = models.ForeignKey(
-        AssetData,
-        on_delete=models.CASCADE,
-        verbose_name=_("Asset"),
-    )
-    scale = models.ForeignKey(
-        ScaleData,
-        on_delete=models.CASCADE,
-        verbose_name=_("Scale"),
-    )
-    value = models.FloatField(
-        verbose_name=_("Value"),
-    )
 
 
 # store Vulnerability
@@ -172,6 +155,11 @@ class RiskData(models.Model):
         on_delete=models.CASCADE,
         verbose_name=_("Service"),
     )
+    asset = models.ForeignKey(
+        AssetData,
+        on_delete=models.CASCADE,
+        verbose_name=_("Threat"),
+    )
     threat = models.ForeignKey(
         ThreatData,
         on_delete=models.CASCADE,
@@ -201,6 +189,9 @@ class RiskData(models.Model):
         blank=False,
         default=RISK_TREATMENT[1][0],
     )
+    max_risk = models.FloatField(
+        verbose_name=_("Maximum risk"),
+    )
 
     class Meta:
         verbose_name_plural = _("Risks")
@@ -209,11 +200,21 @@ class RiskData(models.Model):
 
 # Store the recommendation data
 class RecommendationData(models.Model):
-    name = models.CharField(
+    code = models.CharField(
         max_length=255,
         verbose_name=_("Recommendation name"),
     )
     description = models.TextField(verbose_name=_("Recommendation description"))
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        verbose_name=_("uuid"),
+    )
+    status = models.BooleanField(
+        default=False, verbose_name=_("Status of the recommendation")
+    )
+    due_date = models.DateTimeField(verbose_name=_("Due date"))
 
     class Meta:
         verbose_name_plural = _("Recommendations")
