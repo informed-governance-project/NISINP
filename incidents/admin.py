c1d31ea8af371070ec942dd2660d82344e48ec2b
Juan Rocha <juan.rocha@nc3.lu>
2024-10-16 15:52:04 +0200

    [admin]Rollback to has_permission condition

--- incidents/admin.py
+++ incidents/admin.py
@@ -181,8 +181,8 @@ class PredefinedAnswerAdmin(ExportActionModelAdmin, CustomTranslatableAdmin):
     def render_change_form(
         self, request, context, add=False, change=False, form_url="", obj=None
     ):
-        has_permission = obj and self.has_change_permission(request, obj)
-        if not has_permission:
+        has_permission = obj and not self.has_change_permission(request, obj)
+        if has_permission:
             context.update(
                 {
                     "show_save": False,
@@ -191,7 +191,7 @@ class PredefinedAnswerAdmin(ExportActionModelAdmin, CustomTranslatableAdmin):
                 }
             )
         form = super().render_change_form(request, context, add, change, form_url, obj)
-        if not has_permission:
+        if has_permission:
             form = filter_languages_not_translated(form)
         return form
 
@@ -243,8 +243,8 @@ class QuestionCategoryAdmin(ExportActionModelAdmin, CustomTranslatableAdmin):
     def render_change_form(
         self, request, context, add=False, change=False, form_url="", obj=None
     ):
-        has_permission = obj and self.has_change_permission(request, obj)
-        if not has_permission:
+        has_permission = obj and not self.has_change_permission(request, obj)
+        if has_permission:
             context.update(
                 {
                     "show_save": False,
@@ -253,7 +253,7 @@ class QuestionCategoryAdmin(ExportActionModelAdmin, CustomTranslatableAdmin):
                 }
             )
         form = super().render_change_form(request, context, add, change, form_url, obj)
-        if not has_permission:
+        if has_permission:
             form = filter_languages_not_translated(form)
         return form
 
@@ -350,8 +350,8 @@ class QuestionAdmin(ExportActionModelAdmin, CustomTranslatableAdmin):
     def render_change_form(
         self, request, context, add=False, change=False, form_url="", obj=None
     ):
-        has_permission = obj and self.has_change_permission(request, obj)
-        if not has_permission:
+        has_permission = obj and not self.has_change_permission(request, obj)
+        if has_permission:
             context.update(
                 {
                     "show_save": False,
@@ -360,7 +360,7 @@ class QuestionAdmin(ExportActionModelAdmin, CustomTranslatableAdmin):
                 }
             )
         form = super().render_change_form(request, context, add, change, form_url, obj)
-        if not has_permission:
+        if has_permission:
             form = filter_languages_not_translated(form)
         return form
 
@@ -726,8 +726,8 @@ class WorkflowAdmin(CustomTranslatableAdmin):
     def render_change_form(
         self, request, context, add=False, change=False, form_url="", obj=None
     ):
-        has_permission = obj and self.has_change_permission(request, obj)
-        if not has_permission:
+        has_permission = obj and not self.has_change_permission(request, obj)
+        if has_permission:
             context.update(
                 {
                     "show_save": False,
@@ -736,7 +736,7 @@ class WorkflowAdmin(CustomTranslatableAdmin):
                 }
             )
         form = super().render_change_form(request, context, add, change, form_url, obj)
-        if not has_permission:
+        if has_permission:
             form = filter_languages_not_translated(form)
         return form
 
