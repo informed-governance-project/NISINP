04800bf7ecaa0cb3d7519d0b2c681cdbfa2ce985
Juan Rocha <juan.rocha@nc3.lu>
2024-10-17 11:46:30 +0200

    [views]Adjusted declaration view logic with security measures validation

--- securityobjectives/forms.py
+++ securityobjectives/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 
 class SecurityObjectiveAnswerForm(forms.Form):
@@ -8,7 +9,7 @@ class SecurityObjectiveAnswerForm(forms.Form):
         widget=forms.Textarea(
             attrs={
                 "rows": 2,
-                "placeholder": ""
+                "placeholder": "",
                 # "onblur": "onBlurTextarea(this)",
             }
         ),
@@ -22,4 +23,34 @@ class SecurityObjectiveAnswerForm(forms.Form):
                 # "onblur": "onBlurTextarea(this)",
             }
         ),
+    )
+
+
+class CustomSelect(forms.Select):
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        option = super().create_option(
+            name, value, label, selected, index, subindex=subindex, attrs=attrs
+        )
+
+        if value == "NOT_REVIEWED":
+            option["attrs"]["class"] = "bg-transparent"
+        elif value == "PASS":
+            option["attrs"]["class"] = "bg-success"
+        elif value == "FAIL":
+            option["attrs"]["class"] = "bg-danger"
+
+        return option
+
+
+class SecurityObjectiveStatusForm(forms.Form):
+    security_objective_status = forms.ChoiceField(
+        choices=[
+            ("NOT_REVIEWED", _("Not reviewed")),
+            ("PASS", _("Pass")),
+            ("FAIL", _("Fail")),
+        ],
+        widget=CustomSelect(),
+        initial="NOT_REVIEWED",
     )
--- securityobjectives/views.py
+++ securityobjectives/views.py
@@ -1,12 +1,11 @@
 from collections import defaultdict
 
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count
 from django.shortcuts import render
 from django_otp.decorators import otp_required
 
-from .forms import SecurityObjectiveAnswerForm
-from .models import SecurityMeasure
+from .forms import SecurityObjectiveAnswerForm, SecurityObjectiveStatusForm
+from .models import Standard
 
 
 @login_required
@@ -21,34 +20,26 @@ def get_security_objectives(request):
 def declaration(request):
     """Initialize data for the security objectives declaration."""
     context = {}
-    security_measures = SecurityMeasure.objects.annotate(
-        evidence_count=Count("evidence")
-    ).order_by(
-        "security_objective__position",
+    standard = Standard.objects.all().first()
+    security_objectives_queryset = (
+        standard.securityobjectivesinstandard_set.all().order_by("position")
     )
+    security_objectives = defaultdict(lambda: defaultdict(lambda: []))
 
-    security_objectives = defaultdict(
-        lambda: defaultdict(lambda: {"measures": [], "evidence_count": 0})
-    )
-
-    for measure in security_measures:
-        security_objective = measure.security_objective
-        maturity_level = measure.maturity_level
-        evidences_list = []
-        for evidence in measure.evidence_set.all():
-            evidence.so_answer_form = SecurityObjectiveAnswerForm(
-                prefix=f"evidence_{evidence.id}"
-            )
-            evidences_list.append(evidence)
-
-        measure.evidences = evidences_list
-
-        security_objectives[security_objective][maturity_level]["measures"].append(
-            measure
+    for so_in_standard in security_objectives_queryset:
+        security_objective = so_in_standard.security_objective
+        security_measures = security_objective.securitymeasure_set.all()
+        security_objective.status_form = SecurityObjectiveStatusForm(
+            prefix=f"so_{security_objective.id}"
         )
-        security_objectives[security_objective][maturity_level][
-            "evidence_count"
-        ] += measure.evidence_count
+        for measure in security_measures:
+            security_objective = measure.security_objective
+            maturity_level = measure.maturity_level
+            measure.answer_form = SecurityObjectiveAnswerForm(
+                prefix=f"measure_{measure.id}"
+            )
+
+            security_objectives[security_objective][maturity_level].append(measure)
 
     security_objectives = {
         so: dict(levels) for so, levels in security_objectives.items()
