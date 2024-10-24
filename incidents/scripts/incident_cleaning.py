794e3c1aed3d1d20a73af58872bec258442060f6
jerolomb <jerome.lombardi@cases.lu>
2024-10-24 11:25:22 +0200

    fix #217 adding a new table and mechanism to record deletion scripts actions

--- incidents/scripts/incident_cleaning.py
+++ incidents/scripts/incident_cleaning.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from django.db.models.functions import Now
 from governanceplatform.settings import INCIDENT_RETENTION_TIME_IN_DAY
 from incidents.models import Incident
+from governanceplatform.models import ScriptLogEntry
 
 
 # Script to run once day
@@ -11,5 +12,10 @@ def run():
     incident_to_delete_qs = Incident.objects.filter(
         incident_status='CLOSE',
         incident_notification_date__lte=Now()-timedelta(days=INCIDENT_RETENTION_TIME_IN_DAY)
+    )
+    ScriptLogEntry.objects.create(
+        object_id=None,
+        object_repr="System:Incident script deletion " + str(incident_to_delete_qs.count()) + " incident(s) deleted",
+        action_flag=3,
     )
     incident_to_delete_qs.delete()
