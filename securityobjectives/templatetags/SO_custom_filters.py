7413fbdd05a5ccd6d5e8c90831c16cf0d5de51cd
Juan Rocha <juan.rocha@nc3.lu>
2024-10-29 14:07:47 +0100

    [security_objectives]Removed useless code

--- securityobjectives/templatetags/SO_custom_filters.py
+++ securityobjectives/templatetags/SO_custom_filters.py
@@ -1,47 +1,6 @@
 from django import template
-from django.utils.translation import gettext as _
 
 register = template.Library()
-
-
-@register.filter
-def get_class_name(value):
-    return value.__class__.__name__
-
-
-@register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key)
-
-
-@register.filter
-def index(indexable, i):
-    return indexable[int(i)]
-
-
-@register.filter()
-def translate(text):
-    return _(text)
-
-
-@register.simple_tag
-def get_completion(standard_answer):
-    if standard_answer.is_finished:
-        return _("Finished")
-    else:
-        if standard_answer.securitymeasureanswers.count() == 0:
-            return _("Not started")
-    return _("Work in progress")
-
-
-@register.simple_tag
-def get_url(standard_answer):
-    if standard_answer.is_finished:
-        return '#'
-    else:
-        if standard_answer.securitymeasureanswers.count() == 0:
-            return "create_so/"+str(standard_answer.id)
-    return "create_so/"+str(standard_answer.id)
 
 
 # replace a field in the URL, used for filter + pagination
