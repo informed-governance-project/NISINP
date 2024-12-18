5c6f2594100a1e67fe05db2bb0c2a9ed755be09d
Juan Rocha <juan.rocha@nc3.lu>
2024-12-18 16:51:55 +0100

    [reporting][forms]Removed useless code

--- reporting/forms.py
+++ reporting/forms.py
@@ -9,30 +9,6 @@ from incidents.forms import DropdownCheckboxSelectMultiple
 
 from .models import ObservationRecommendation, SectorReportConfiguration
 
-# from .models import RiskAnalysisJson
-
-
-# to upload the JSON
-# class RiskAnalysisSubmissionForm(forms.Form):
-#     files = forms.FileField(
-#         widget=forms.TextInput(
-#             attrs={
-#                 "type": "File",
-#                 "multiple": "True",
-#             }
-#         )
-#     )
-
-# class Meta:
-#     model = RiskAnalysisJson
-#     fields = ["data"]
-#     labels = {
-#         "data": _("Upload JSON File"),
-#     }
-#     widgets = {
-#         "data": forms.FileInput(attrs={"accept": ".json"}),
-#     }
-
 
 class ImportRiskAnalysisForm(forms.Form):
     import_file = forms.FileField(
