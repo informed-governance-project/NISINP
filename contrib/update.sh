6a7b09209d386bc997492a05faa64da91ca22a44
Rémi Laurent <remi.laurent@conostix.com>
2024-12-19 15:36:41 +0100

    allow tag selection in update.sh script

--- contrib/update.sh
+++ contrib/update.sh
@@ -8,10 +8,13 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+COMMITID="${1:-master}"
+
 set -e
 #set -x
 
-git pull origin master --tags
+git fetch origin --tags
+git checkout "$COMMITID"
 npm ci
 poetry install --only main
 poetry run python manage.py collectstatic --no-input
