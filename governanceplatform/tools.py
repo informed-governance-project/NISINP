1a9e15ee82cea05ba25cc6924d2555cf1f0929e1
Juan Rocha <juan.rocha@nc3.lu>
2025-03-04 16:40:23 +0100

    [version] Refactored get_version function to improve readability and streamline version retrieval logic

--- governanceplatform/tools.py
+++ governanceplatform/tools.py
@@ -1,6 +1,5 @@
-import subprocess
 import os
-
+import subprocess
 from importlib.metadata import PackageNotFoundError, version
 
 from governanceplatform.settings import BASE_DIR
@@ -12,9 +11,9 @@ def get_version():
     on the project home page.
     Try to get the version from the Git tags.
     """
-    version_res = ''
-    if 'APP_VERSION' in os.environ:
-        version_res = os.getenv('APP_VERSION')
+    version_res = ""
+    if "APP_VERSION" in os.environ:
+        version_res = os.getenv("APP_VERSION")
     else:
         version_res = (
             subprocess.run(
@@ -30,17 +29,10 @@ def get_version():
         except PackageNotFoundError:
             version_res = ""
 
-    version_parts = version_res.split("-")
-    if len(version_parts) == 1:
-        app_version = version_parts[0]
-        version_url = "https://github.com/informed-governance-project/NISINP/releases/tag/{}".format(
-            version_parts[0]
-        )
     else:
-        app_version = f"{version_parts[0]} - {version_parts[2][1:]}"
-        version_url = (
-            "https://github.com/informed-governance-project/NISINP/commits/{}".format(
-                version_parts[2][1:]
-            )
+        app_version = version_res
+        version_url = "https://github.com/informed-governance-project/NISINP/releases/tag/{}".format(
+            version_res
         )
+
     return {"app_version": app_version, "version_url": version_url}
