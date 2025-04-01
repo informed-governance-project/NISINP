15e4687dd0a43fb2449b835deacc9bd9102bde97
Juan Rocha <juan.rocha@nc3.lu>
2025-04-01 10:41:45 +0200

    [localization] Added verbose names for Reporting and Security Objectives apps

--- reporting/apps.py
+++ reporting/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ReportingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "reporting"
+    verbose_name = _("Reporting")
 
     def ready(self):
         from . import signals  # noqa: F401
--- securityobjectives/apps.py
+++ securityobjectives/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class SecurityobjectivesConfig(AppConfig):
-    name = 'securityobjectives'
+    name = "securityobjectives"
+    verbose_name = _("Security objectives")
