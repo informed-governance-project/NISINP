371fc96577867fd555bc110ed92e46cc23524803
Juan Rocha <juan.rocha@nc3.lu>
2025-06-04 15:41:44 +0200

    [celery]Initialized Celery app and configure settings in governanceplatform

--- governanceplatform/__init__.py
+++ governanceplatform/__init__.py
@@ -1,3 +1,8 @@
+from celery import Celery
+
 from governanceplatform import tools
 
 __version__ = tools.get_version()
+
+app = Celery("governanceplatform")
+app.config_from_object("django.conf:settings", namespace="CELERY")
