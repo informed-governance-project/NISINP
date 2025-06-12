6d3ee7e5bb8911521942a136babc35757349f3d2
Juan Rocha <juan.rocha@nc3.lu>
2025-06-12 11:23:18 +0200

    [logging]Added logging configuration to increase weasyprint log level

--- reporting/tasks.py
+++ reporting/tasks.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import os
 import shutil
 import uuid
@@ -14,6 +15,11 @@ from weasyprint import CSS, HTML
 
 from .helpers import create_entry_log, get_charts, get_risk_data, get_so_data
 from .models import CompanyReporting, GeneratedReport
+
+# Increasing weasyprint log level
+for logger_name in ["weasyprint", "fontTools"]:
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.ERROR)
 
 
 @shared_task
