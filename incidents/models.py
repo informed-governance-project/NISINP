1abfc6765eae259aab92599e25e987619bec2e8f
jerolomb <jerome.lombardi@cases.lu>
2025-06-16 07:59:03 +0200

    [incident] fix typo in question options comment #392

--- incidents/models.py
+++ incidents/models.py
@@ -971,7 +971,7 @@ class QuestionOptions(models.Model):
     )
     # creation date by default before the creation of app
     updated_at = models.DateTimeField(
-        verbose_name=_("Created at"), default=datetime(2000, 1, 1)
+        verbose_name=_("Updated at"), default=datetime(2000, 1, 1)
     )
     historic = models.ManyToManyField(QuestionOptionsHistory, blank=True)
     deleted_date = models.DateTimeField(
