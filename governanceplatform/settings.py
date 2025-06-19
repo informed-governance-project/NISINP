c7d9536e811e36931d5e4b6a9a79ad1c491018f0
Juan Rocha <juan.rocha@nc3.lu>
2025-06-19 09:48:06 +0200

    [settings]Added CELERY and REDIS settings

--- governanceplatform/settings.py
+++ governanceplatform/settings.py
@@ -479,6 +479,26 @@ try:
 except AttributeError:
     EMAIL_FOR_CONTACT = ""
 
+# CELERY configuration
+try:
+    CELERY_BROKER_URL = config.CELERY_BROKER_URL
+    CELERY_RESULT_BACKEND = config.CELERY_RESULT_BACKEND
+except AttributeError:
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
+    CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
+
+try:
+    CELERY_ACCEPT_CONTENT = config.CELERY_ACCEPT_CONTENT
+    CELERY_TASK_SERIALIZER = config.CELERY_TASK_SERIALIZER
+except AttributeError:
+    CELERY_ACCEPT_CONTENT = ["json"]
+    CELERY_TASK_SERIALIZER = "json"
+
+# Paths for deliveries
+try:
+    PATH_FOR_REPORTING_PDF = config.PATH_FOR_REPORTING_PDF
+except AttributeError:
+    PATH_FOR_REPORTING_PDF = f"{os.getcwd()}/tmp/"
 # Email adress for FROM field and RETURN-PATH
 try:
     EMAIL_CONTACT_FROM = config.EMAIL_CONTACT_FROM
