21ea025f9928caf9374abebd3b769712740e1f19
jerolomb <jerome.lombardi@cases.lu>
2025-06-27 11:35:00 +0200

    [API] add finest permission

--- governanceplatform/api/v1/permissions.py
+++ governanceplatform/api/v1/permissions.py
@@ -40,3 +40,73 @@ class GroupBasedPermission(BasePermission):
         if not allowed_write_groups:
             return False
         return user.groups.filter(name__in=allowed_write_groups).exists()
+
+
+class IsRegulatorCreatorModificationOrReadOnly(BasePermission):
+    """
+    GET HEAD OPTION for all authenticated user
+    POST/PUT/PATCH/DELETE only for the creator
+    """
+
+    def has_permission(self, request, view):
+        # not allowed for unauthentified
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # Read for everyone
+        if request.method in SAFE_METHODS:
+            return True
+
+        creator = obj.creator
+        regulator = request.user.regulators.first()
+
+        return creator == regulator
+
+
+class CreatorAndActionGroupPermission(BasePermission):
+    """
+    Authorize DRF action depending on group defined on views
+    Exemple : allowed_groups = {
+        "list": ["OperatorUser", "OperatorAdmin"],
+        "create": ["RegulatorAdmin"],
+        "update": ["RegulatorAdmin"],
+        "destroy": ["RegulatorAdmin", "is_creator"],
+    }
+    is_creator to filter on the object creator or not
+    if an action is not present in allowed group, it's denied by default
+    list	GET on /
+    retrieve	GET on /<id>/
+    create	POST
+    update	PUT
+    partial_update	PATCH
+    destroy	DELETE
+    """
+
+    def has_permission(self, request, view):
+        user = request.user
+        if not user or not user.is_authenticated:
+            return False
+
+        action = getattr(view, "action", None)
+        allowed_groups = getattr(view, "allowed_groups", {})
+
+        # refused if action is not defined
+        if action not in allowed_groups:
+            return False
+
+        # Authorised if the user belong to at least one group
+        return user.groups.filter(name__in=allowed_groups[action]).exists()
+
+    def has_object_permission(self, request, view, obj):
+        action = getattr(view, "action", None)
+        allowed_groups = getattr(view, "allowed_groups", {})
+        if action not in allowed_groups:
+            return False
+
+        if "is_creator" in allowed_groups[action]:
+            creator = obj.creator
+            regulator = request.user.regulators.first()
+
+            return creator == regulator
+        else:
+            return True
--- governanceplatform/api/v1/views.py
+++ governanceplatform/api/v1/views.py
@@ -1,13 +1,26 @@
 from rest_framework import viewsets
 from governanceplatform.models import Sector
 from .serializers import SectorSerializer
-from .permissions import GroupBasedPermission
+from .permissions import CreatorAndActionGroupPermission
 
 
 class SectorViewSet(viewsets.ModelViewSet):
     queryset = Sector.objects.all()
     serializer_class = SectorSerializer
-    permission_classes = [GroupBasedPermission]
+    permission_classes = [CreatorAndActionGroupPermission]
 
-    write_groups = ["regulatorAdmin"]
-    read_groups = ["regulatorAdmin", "regulatorUser"]
+    write_groups = ["RegulatorAdmin"]
+    read_groups = ["RegulatorAdmin", "RegulatorUser"]
+
+    allowed_groups = {
+        "list": [
+            "RegulatorAdmin", "RegulatorUser", "is_creator",
+        ],
+        "retrieve": [
+            "RegulatorAdmin", "RegulatorUser", "is_creator",
+        ],
+        "create": ["RegulatorAdmin"],
+        "update": ["RegulatorAdmin"],
+        "partial_update": ["RegulatorAdmin"],
+        "destroy": ["RegulatorAdmin"],
+    }
