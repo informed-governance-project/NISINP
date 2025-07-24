9e3362ed87a857d8da00be932681a73c200d4e71
Juan Rocha <juan.rocha@nc3.lu>
2025-07-24 12:33:29 +0200

    [incidents] Added filter by company in use, when user is OperatorUser

--- incidents/views.py
+++ incidents/views.py
@@ -102,25 +102,21 @@ def get_incidents(request):
     elif is_observer_user(user):
         html_view = "observer/incidents.html"
         incidents = user.observers.first().get_incidents()
-    elif user_in_group(user, "OperatorAdmin") or user_in_group(user, "OperatorUser"):
-        # OperatorAdmin can see all the reports of the selected company if he is administrator:
-        is_administrator = CompanyUser.objects.filter(
-            company__id=request.session.get("company_in_use"),
-            user=user,
-            is_company_administrator=True,
-        ).count()
-        if is_administrator > 0:
-            query1 = incidents.filter(
-                company__id=request.session.get("company_in_use")
-            )
-        else:
-            query1 = incidents.filter(
-                company__id=request.session.get("company_in_use"),
-                affected_sectors__in=user.companyuser_set.all()
-                .distinct()
-                .values_list("sectors", flat=True),
-            )
-        query2 = incidents.filter(contact_user=user)
+        f = IncidentFilter(incidents_filter_params, queryset=incidents.order_by("id"))
+    elif user_in_group(user, "OperatorAdmin"):
+        # OperatorAdmin can see all the reports of the selected company.
+        incidents = incidents.filter(company__id=request.session.get("company_in_use"))
+        f = IncidentFilter(incidents_filter_params, queryset=incidents)
+    elif user_in_group(user, "OperatorUser"):
+        # OperatorUser see his incident and the one oh his sectors for the company
+        company_in_use = request.session.get("company_in_use")
+        query1 = incidents.filter(
+            company__id=company_in_use,
+            affected_sectors__in=user.companyuser_set.filter(company__id=company_in_use)
+            .distinct()
+            .values_list("sectors", flat=True),
+        )
+        query2 = incidents.filter(company__id=company_in_use, contact_user=user)
         incidents = (query1 | query2).distinct()
         f = IncidentFilter(incidents_filter_params, queryset=incidents)
     else:
