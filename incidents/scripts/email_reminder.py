c72362a82434f47934b00865cf7228142a5016d8
jerolomb <jerome.lombardi@cases.lu>
2025-08-07 12:53:12 +0200

    [script] temporary fix for script comment #425

--- incidents/scripts/email_reminder.py
+++ incidents/scripts/email_reminder.py
@@ -20,56 +20,57 @@ def run(logger=logger):
     # for all unclosed incident
     actual_time = timezone.now()
     for incident in Incident.objects.filter(incident_status="GOING"):
-        # Workflow with deadline from prev workflow
-        for incident_workflow in incident.get_latest_incident_workflows():
-            # chek if there is a next workflow
-            next_workflow = incident_workflow.get_next_workflow()
-            if next_workflow is not False:
-                next_incident_workflow = (
-                    IncidentWorkflow.objects.all()
-                    .filter(
-                        incident=incident,
-                        workflow=next_workflow,
-                    )
-                    .order_by("-timestamp")
-                    .first()
-                )
-                # there is one next workflow but not filled
-                if next_incident_workflow is None:
-                    next_sector_regulation_workflow = (
-                        SectorRegulationWorkflow.objects.all()
+        if incident.sector_regulation is not None:
+            # Workflow with deadline from prev workflow
+            for incident_workflow in incident.get_latest_incident_workflows():
+                # chek if there is a next workflow
+                next_workflow = incident_workflow.get_next_workflow()
+                if next_workflow is not False:
+                    next_incident_workflow = (
+                        IncidentWorkflow.objects.all()
                         .filter(
-                            sector_regulation=incident.sector_regulation,
+                            incident=incident,
                             workflow=next_workflow,
                         )
+                        .order_by("-timestamp")
                         .first()
                     )
-                    emails = SectorRegulationWorkflowEmail.objects.all().filter(
-                        sector_regulation_workflow=next_sector_regulation_workflow,
-                        trigger_event="PREV_WORK",
+                    # there is one next workflow but not filled
+                    if next_incident_workflow is None:
+                        next_sector_regulation_workflow = (
+                            SectorRegulationWorkflow.objects.all()
+                            .filter(
+                                sector_regulation=incident.sector_regulation,
+                                workflow=next_workflow,
+                            )
+                            .first()
+                        )
+                        emails = SectorRegulationWorkflowEmail.objects.all().filter(
+                            sector_regulation_workflow=next_sector_regulation_workflow,
+                            trigger_event="PREV_WORK",
+                        )
+                        for email in emails:
+                            dt = actual_time - incident_workflow.timestamp
+                            if (
+                                math.floor(dt.total_seconds() / 60 / 60)
+                                == email.delay_in_hours
+                            ):
+                                send_email(email.email, incident)
+                # From notification date
+                sector_regulation_workflow = (
+                    SectorRegulationWorkflow.objects.all()
+                    .filter(
+                        sector_regulation=incident.sector_regulation,
+                        workflow=incident_workflow.workflow,
                     )
-                    for email in emails:
-                        dt = actual_time - incident_workflow.timestamp
-                        if (
-                            math.floor(dt.total_seconds() / 60 / 60)
-                            == email.delay_in_hours
-                        ):
-                            send_email(email.email, incident)
-            # From notification date
-            sector_regulation_workflow = (
-                SectorRegulationWorkflow.objects.all()
-                .filter(
-                    sector_regulation=incident.sector_regulation,
-                    workflow=incident_workflow.workflow,
+                    .first()
                 )
-                .first()
-            )
 
-            emails = SectorRegulationWorkflowEmail.objects.all().filter(
-                sector_regulation_workflow=sector_regulation_workflow,
-                trigger_event="NOTIF_DATE",
-            )
-            for email in emails:
-                dt = actual_time - incident_workflow.timestamp
-                if math.floor(dt.total_seconds() / 60 / 60) == email.delay_in_hours:
-                    send_email(email.email, incident)
+                emails = SectorRegulationWorkflowEmail.objects.all().filter(
+                    sector_regulation_workflow=sector_regulation_workflow,
+                    trigger_event="NOTIF_DATE",
+                )
+                for email in emails:
+                    dt = actual_time - incident_workflow.timestamp
+                    if math.floor(dt.total_seconds() / 60 / 60) == email.delay_in_hours:
+                        send_email(email.email, incident)
--- incidents/scripts/workflow_update_status.py
+++ incidents/scripts/workflow_update_status.py
@@ -19,21 +19,22 @@ def run(logger=logger):
     ongoing_incidents = Incident.objects.filter(incident_status="GOING")
     for incident in ongoing_incidents:
         filled_workflow_ids = IncidentWorkflow.objects.filter(incident=incident).values_list("workflow", flat=True)
-        unfilled_report_ids = incident.sector_regulation.workflows.exclude(
-            id__in=filled_workflow_ids
-        ).values_list("id", flat=True)
-        srw = SectorRegulationWorkflow.objects.filter(
-            workflow__in=unfilled_report_ids,
-            sector_regulation=incident.sector_regulation,
-        )
-        for report in srw:
-            delay_in_hours = report.delay_in_hours_before_deadline
+        if incident.sector_regulation is not None:
+            unfilled_report_ids = incident.sector_regulation.workflows.exclude(
+                id__in=filled_workflow_ids
+            ).values_list("id", flat=True)
+            srw = SectorRegulationWorkflow.objects.filter(
+                workflow__in=unfilled_report_ids,
+                sector_regulation=incident.sector_regulation,
+            )
+            for report in srw:
+                delay_in_hours = report.delay_in_hours_before_deadline
 
-            dt = report.how_late_is_the_report(incident, actual_time)
+                dt = report.how_late_is_the_report(incident, actual_time)
 
-            if dt and math.floor(dt.total_seconds() / 60 / 60) == delay_in_hours:
-                if incident.sector_regulation.report_status_changed_email:
-                    send_email(
-                        incident.sector_regulation.report_status_changed_email,
-                        incident,
-                    )
+                if dt and math.floor(dt.total_seconds() / 60 / 60) == delay_in_hours:
+                    if incident.sector_regulation.report_status_changed_email:
+                        send_email(
+                            incident.sector_regulation.report_status_changed_email,
+                            incident,
+                        )
