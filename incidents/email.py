025d4bdd02ae99f5f5e1822cf09848a0c11d7337
jerolomb <jerome.lombardi@cases.lu>
2025-08-26 13:59:06 +0200

    [NI] comment #183 handled missing date in email sending

--- incidents/email.py
+++ incidents/email.py
@@ -40,15 +40,27 @@ def replace_email_variables(content, incident):
         elif variable == "#INCIDENT_DETECTION_DATE#":
             last_report = incident.get_latest_incident_workflow()
             if not last_report:
-                var_txt = incident.incident_detection_date.strftime("%Y-%m-%d")
+                var_txt = (
+                    incident.incident_detection_date.strftime("%Y-%m-%d")
+                    if incident.incident_detection_date is not None
+                    else ""
+                )
             else:
-                var_txt = last_report.report_timeline.incident_detection_date.strftime("%Y-%m-%d")
+                var_txt = (
+                    last_report.report_timeline.incident_detection_date.strftime("%Y-%m-%d")
+                    if last_report.report_timeline.incident_detection_date is not None
+                    else ""
+                )
         elif variable == "#INCIDENT_STARTING_DATE#":
             last_report = incident.get_latest_incident_workflow()
             if not last_report:
                 var_txt = ""
             else:
-                var_txt = last_report.report_timeline.incident_starting_date.strftime("%Y-%m-%d")
+                var_txt = (
+                    last_report.report_timeline.incident_starting_date.strftime("%Y-%m-%d")
+                    if last_report.report_timeline.incident_starting_date is not None
+                    else ""
+                )
         else:
             var_txt = (
                 getattr(incident, key) if getattr(incident, key) is not None else ""
