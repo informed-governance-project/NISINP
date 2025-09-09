ae3cf78f3590a26357bf097649ba495a8c099e6a
jerolomb <jerome.lombardi@cases.lu>
2025-09-09 08:58:57 +0200

    [Gov][NI] only propose the approved company in the company select box comment #469

--- governanceplatform/views.py
+++ governanceplatform/views.py
@@ -143,7 +143,12 @@ def activate_account(request, token):
 
 def select_company(request):
     if request.method == "POST":
-        form = SelectCompany(request.POST, companies=request.user.companies.distinct())
+        form = SelectCompany(
+            request.POST,
+            companies=request.user.companies.filter(
+                companyuser__approved=True
+            ).distinct(),
+        )
 
         if form.is_valid() and request.user.is_authenticated:
             user_company = Company.objects.get(
