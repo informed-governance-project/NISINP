c955d0f55b554429fec7fd451c27e8859102626b
Juan Rocha <juan.rocha@nc3.lu>
2025-09-11 13:28:41 +0200

    [SO][filter_tags]Added missing conditions when is regulator, #452

--- securityobjectives/templatetags/SO_custom_filters.py
+++ securityobjectives/templatetags/SO_custom_filters.py
@@ -18,20 +18,22 @@ def settings_value(name):
     return getattr(settings, name, "")
 
 
-@register.simple_tag
-def status_class(value):
+@register.simple_tag(takes_context=True)
+def status_class(context, value):
     if value == "PASS":
-        return "border border-passed border-2"
+        if context["is_regulator"]:
+            return "border border-passed border-2"
+        return "bg-passed"
     elif value == "PASSM":
         return "bg-passed"
     elif value == "FAIL":
-        return "border border-failed border-2"
+        if context["is_regulator"]:
+            return "border border-failed border-2"
+        return "bg-failed "
     elif value == "FAILM":
         return "bg-failed "
     elif value == "DELIV":
         return "bg-under-review"
-    elif value == "OUT":
-        return "table-dark"
     else:
         return "bg-unsubmitted"
 
@@ -39,20 +41,22 @@ def status_class(value):
 @register.simple_tag(takes_context=True)
 def status_icon(context, value):
     if value == "PASS":
-        return "custom-icon-passed"
+        if context["is_regulator"]:
+            return "custom-icon-passed"
+        return "custom-icon-passed-white"
     elif value == "PASSM":
         if context["is_regulator"]:
             return "custom-icon-passed-sent"
         return "custom-icon-passed-white"
     elif value == "FAIL":
-        return "custom-icon-failed"
+        if context["is_regulator"]:
+            return "custom-icon-failed"
+        return "custom-icon-failed-white"
     elif value == "FAILM":
         if context["is_regulator"]:
             return "custom-icon-failed-sent"
         return "custom-icon-failed-white"
     elif value == "DELIV":
         return "custom-icon-under-review-white"
-    elif value == "OUT":
-        return "table-dark"
     else:
         return "custom-icon-unsubmitted-white"
