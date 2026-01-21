4b62caaa91044d8b16ee221cd93b8014405f1c2f
Juan Rocha <juan.rocha@nc3.lu>
2026-01-21 13:39:37 +0100

    [GOV]Removed useless mixin, comment #614

--- securityobjectives/mixins.py
+++ securityobjectives/mixins.py
@@ -14,17 +14,3 @@ class ImportMixin:
         except Exception:
             pass
         super().save_model(request, obj, form, change)
-
-    def get_model_perms(self, request):
-        user = request.user
-        functionalities = None
-        if user.regulators.first() is not None:
-            functionalities = user.regulators.first().functionalities
-        if user.observers.first() is not None:
-            functionalities = user.observers.first().functionalities
-        if functionalities is not None:
-            if "securityobjectives" in functionalities.all().values_list(
-                "type", flat=True
-            ):
-                return {"change": True, "add": True}
-        return {"change": False, "add": False}
