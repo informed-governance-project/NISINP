615594e3b9447a0ae902fa62f4dda7c02b53444f
Juan Rocha <juan.rocha@nc3.lu>
2026-02-05 10:46:05 +0100

    [SO][Globals]Added ALLOWED_SORT_FIELDS, comment #588

--- securityobjectives/globals.py
+++ securityobjectives/globals.py
@@ -12,3 +12,16 @@ STANDARD_ANSWER_REVIEW_STATUS = [
 SO_EMAIL_VARIABLES = [
     ("#SO_REFERENCE#", "group_id"),
 ]
+
+ALLOWED_SORT_FIELDS = {
+    "status": "status",
+    "last_update": "last_update",
+    "submit_date": "submit_date",
+    "identifier": "group__group_id",
+    "framework": "standard__translations__label",
+    "company_name": "creator_company_name",
+    "creator": "creator_name",
+    "sectors": "sectors__translations__name",
+    "year": "year_of_submission",
+    "reviewed_percentage": "reviewed_percentage",
+}
