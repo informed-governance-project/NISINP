c97583840590a2d50abe277389b13b882dce1bf2
Juan Rocha <juan.rocha@nc3.lu>
2026-03-04 15:37:47 +0100

    [reporting][globals]Removed SO_COLOR_PALETTE, comment #639

--- reporting/globals.py
+++ reporting/globals.py
@@ -10,13 +10,6 @@ RISK_TREATMENT = [
 
 SECTOR_LEGEND = _("Sector average")
 
-SO_COLOR_PALETTE = [
-    (0, "#ED2939"),
-    (1, "#FFC000"),
-    (2, "#DDE96D"),
-    (3, "#00B050"),
-]
-
 CHARTS_COLOR_PALETTE = [
     ("#023255", "#0063B2"),
     ("#EF7D00", "#F3BC3E"),
