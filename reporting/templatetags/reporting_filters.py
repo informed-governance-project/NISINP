d1e0f42f9624a610783b6bcb0e56382dc0b5d3f6
Juan Rocha <juan.rocha@nc3.lu>
2026-03-06 08:28:37 +0100

    [reporting][filters_tags]Cleaned up, comment #639

--- reporting/templatetags/reporting_filters.py
+++ reporting/templatetags/reporting_filters.py
@@ -1,31 +1,6 @@
 from django import template
-from django.conf import settings
 
 register = template.Library()
-
-
-@register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key)
-
-
-@register.filter
-def index(indexable, i):
-    try:
-        return indexable[int(i)]
-    except (IndexError, ValueError):
-        return None
-
-
-# get settings value
-@register.simple_tag
-def settings_value(name):
-    return getattr(settings, name, "")
-
-
-@register.filter
-def range_list(value):
-    return range(int(value))
 
 
 @register.simple_tag
