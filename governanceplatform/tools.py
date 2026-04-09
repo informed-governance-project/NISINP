d0b94b14b5e5f0df61e8d914f176f5fe76c2371f
Juan Rocha <juan.rocha@nc3.lu>
2026-04-09 13:18:15 +0200

    [docker]Removed git dependencies

--- governanceplatform/tools.py
+++ governanceplatform/tools.py
@@ -1,14 +1,11 @@
 import os
-import subprocess
 from importlib.metadata import PackageNotFoundError, version
-
-from governanceplatform.settings import BASE_DIR
 
 
 def get_version():
     """
     Returns a dictionary containing:
-    - The app version (from environment or Git tag),
+    - The app version (from environment variable or fallback to pyproject.toml),
     - The version URL on GitHub,
     - The version from pyproject.toml (or fallback).
     """
@@ -17,31 +14,16 @@ def get_version():
     try:
         app_version_from_pyproject = version("governanceplatform")
     except PackageNotFoundError:
-        app_version_from_pyproject = "inconnue"
+        app_version_from_pyproject = "unknown"
 
-    # Try to get version from env or Git tags
+    # Try to get version from env
     app_version = os.environ.get("APP_VERSION")
-
-    if not app_version:
-        try:
-            result = subprocess.run(
-                ["git", "-C", BASE_DIR, "describe", "--tags"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.DEVNULL,
-                check=True,
-            )
-            app_version = result.stdout.decode().strip()
-        except subprocess.SubprocessError:
-            app_version = ""
 
     # Final fallback to pyproject version if Git version is unavailable
     if not app_version:
         app_version = f"v{app_version_from_pyproject}"
 
-    version_url = f"https://github.com/informed-governance-project/NISINP/releases/tag/{app_version}"
-
     return {
         "app_version": app_version,
-        "version_url": version_url,
         "app_version_from_pyproject": app_version_from_pyproject,
     }
