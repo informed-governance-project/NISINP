3330aefe12e800b2333af272a0d4de14a29b1c9a
Juan Rocha <juan.rocha@nc3.lu>
2026-04-22 09:32:57 +0200

    [settings] Add 'django.contrib.postgres' to INSTALLED_APPS, comment #635

--- governanceplatform/settings.py
+++ governanceplatform/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    "django.contrib.postgres",
     "django_extensions",
     "governanceplatform",
     "incidents",
