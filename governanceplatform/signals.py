27f5a10f5c69c8f924a9d8d1ed3283cfe2443819
Juan Rocha <juan.rocha@nc3.lu>
2026-05-07 09:16:35 +0200

    fix: ensure regulator user is active when updating groups

--- governanceplatform/signals.py
+++ governanceplatform/signals.py
@@ -158,6 +158,7 @@ def update_regulator_user_groups(sender, instance, created, **kwargs):
     user = instance.user
     user.is_staff = False
     user.is_superuser = False
+    user.is_active = True
 
     # Regulator Administrator permissions
     if instance.is_regulator_administrator:
